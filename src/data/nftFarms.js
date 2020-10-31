23bbbc6c977d1bdbed88db550a26c044b677f745
Cybourgeoisie <theocarina@gmail.com>
2020-10-31 15:54:34 -0400

    Add mainnet contracts

--- src/data/nftFarms.js
+++ src/data/nftFarms.js
@@ -18,15 +18,15 @@ export default {
 		"niftex" : "https://www.niftex.com/launches/details/SKULL",
 		"opensea" : "https://opensea.io/assets/0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0/14914",
 		"farmContract" : {
-			"mainnet" : "",
+			"mainnet" : "0xcdB2a49667c1b3Eeb083159ea3bDb6dc1D5aBecf",
 			"rinkeby" : "0x03e156e08F8596ea0ba0A9B72F2E81c0Ad7D946F"
 		},
 		"shardContract" : {
-			"mainnet" : "",
+			"mainnet" : "0xcCE50fD7f41574AdB8fA600055B74303111f2e40",
 			"rinkeby" : "0x74CEa454b9CeE39D18C1A319bf2749388fe33945"
 		},
 		"tokenContract" : {
-			"mainnet" : "",
+			"mainnet" : "0x87b008E57F640D94Ee44Fd893F0323AF933F9195",
 			"rinkeby" : "0x34bFe1342768D6553C970c826392B20EbC0411E2"
 		}
 	},
@@ -39,15 +39,15 @@ export default {
 		"niftex" : "https://www.niftex.com/launches/details/WNDR",
 		"opensea" : "https://opensea.io/assets/0x22adcaf1d6b5213a76bfddab6d4d38c25ce7c029/73",
 		"farmContract" : {
-			"mainnet" : "",
+			"mainnet" : "0x606517B453Fa0bfE8a39bda2d3401fF543506355",
 			"rinkeby" : "0xDbeD1819c165Af8395d0a0b9a779EB70316a464B"
 		},
 		"shardContract" : {
-			"mainnet" : "",
+			"mainnet" : "0x783E2dcFbf6F40c6943d9AdeD6113C7013c46c9b",
 			"rinkeby" : "0xbe2e11f1fA79AbB81d1f37D4ae8916ee50A00676"
 		},
 		"tokenContract" : {
-			"mainnet" : "",
+			"mainnet" : "0xED7Fa212E100DFb3b13B834233E4B680332a3420",
 			"rinkeby" : "0x3c8fbce8ed206e80aa5dfc45c8a2bcfc54235e9f"
 		}
 	},
@@ -60,15 +60,15 @@ export default {
 		"niftex" : "https://www.niftex.com/launches/details/FRAME",
 		"opensea" : "https://opensea.io/assets/0x1702bea2755d076adf737b785926b086eff36f75/212",
 		"farmContract" : {
-			"mainnet" : "",
+			"mainnet" : "0xD4639AE0A59287a72d27D22bD374d848cbb798DB",
 			"rinkeby" : "0x8AdfEcf3A3ef16e0Fb397466290146fCD289d999"
 		},
 		"shardContract" : {
-			"mainnet" : "",
+			"mainnet" : "0xA69fCAaA3F5AE04E63a7FeC7Aff9277BC0BDf8B4",
 			"rinkeby" : "0xF8ba140e1683C0B7C37b9583f8765A72476121E3"
 		},
 		"tokenContract" : {
-			"mainnet" : "",
+			"mainnet" : "0x0f46eba0bafe2739d9f3c43a48bb84bee1f6644f",
 			"rinkeby" : "0x0a0Cc964f3414672c82a13d726F0228AC4331dBe"
 		}
 	}
