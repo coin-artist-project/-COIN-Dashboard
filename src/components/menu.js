6af1f90f1fb366aa2c8e3ee1e5b1a26e6b9a7fe4
Cr0wn-Gh0ul <troy.salem.work@gmail.com>
2021-04-25 12:13:33 -0400

    dont show menu / remove roadmap

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,51 +1,61 @@
-import React, { Component } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from "react-router-dom";
 import { push as Menu } from "react-burger-menu";
 import { Image } from "semantic-ui-react";
 import PT from "../images/PalmTreeLarge.png"
+import { StoreContext } from "../store/store.js";
 
 
-class BurgerMenu extends Component {
-    constructor(props) {
-        super();
-        this.state = {
-            'menuOpen': false
+function BurgerMenu() {
+    const { store, actions } = useContext(StoreContext);
+    const [menuOpen, setMenu] = useState(false)
+
+    const handleStateChange = (state) => {
+        setMenu(state.isOpen)
+    }
+
+    const hasBal = () => {
+        if (store.wallet && store.wallet.balances) {
+            for (let i = 0; i < store.wallet.balances.length; i++) {
+                if (store.wallet.balances[i] > 0 || parseInt(store.wallet.balances[i]) > 0) {
+                    return true
+                }
+            }
         }
+        return false;
     }
-    closeMenu() {
-        this.setState({ menuOpen: false })
-    }
-    handleStateChange(state) {
-        this.setState({ menuOpen: state.isOpen });
-    }
-    render() {
-        return (
-            <Menu isOpen={this.state.menuOpen} burgerButtonClassName={this.state.menuOpen ? "bm-burger-button-hidden" : "bm-burger-button"} onStateChange={(state) => this.handleStateChange(state)}>
-                <Image className="menuLogo" centered={true} src={PT} size="small" />
 
-                <Link id="home" onClick={() => this.closeMenu()} className="menu-item" to={"/"}>
-                    {">"} Home
+    return (
+        <Menu isOpen={menuOpen} burgerButtonClassName={menuOpen ? "bm-burger-button-hidden" : "bm-burger-button"} onStateChange={(menuOpen) => handleStateChange(menuOpen)}>
+            <Image className="menuLogo" centered={true} src={PT} size="small" />
+
+            <Link id="home" onClick={() => setMenu(false)} className="menu-item" to={"/"}>
+                {">"} Home
                 </Link>
-                <Link id="calm" onClick={() => this.closeMenu()} className="menu-item" to={"/calm"}>
-                    {">"} C.A.L.M
+            {!hasBal() ? (<></>) : (<>
+            <Link id="calm" onClick={() => setMenu(false)} className="menu-item" to={"/calm"}>
+                {">"} C.A.L.M
                 </Link>
-                <Link id="farm" onClick={() => this.closeMenu()} className="menu-item" to={"/farm"}>
-                    {">"} NFT Farms
+            <Link id="farm" onClick={() => setMenu(false)} className="menu-item" to={"/farm"}>
+                {">"} NFT Farms
                 </Link>
-                <Link id="charter" onClick={() => this.closeMenu()} className="menu-item" to={"/charter"}>
-                    {">"} Charter
+            </>)}
+            <Link id="charter" onClick={() => setMenu(false)} className="menu-item" to={"/charter"}>
+                {">"} Charter
                 </Link>
-                <Link id="news" onClick={() => this.closeMenu()} className="menu-item" to={"/news"}>
-                    {">"} News
+            <Link id="news" onClick={() => setMenu(false)} className="menu-item" to={"/news"}>
+                {">"} News
                 </Link>
-                <Link id="puzzleInfo" onClick={() => this.closeMenu()} className="menu-item" to={"/puzzleInfo"}>
-                    {">"} Puzzle Dev Guide
+            <Link id="puzzleInfo" onClick={() => setMenu(false)} className="menu-item" to={"/puzzleInfo"}>
+                {">"} Puzzle Dev Guide
                 </Link>
-                <Link id="roadmap" className="menu-item soon" >
-                    <em>{">"} Roadmap (Coming soon)</em>
-                </Link>
-            </Menu>
-        )
-    }
+                {true ? (<></>) : (<>
+            <Link id="roadmap" className="menu-item soon" >
+                <em>{">"} Roadmap (Coming soon)</em>
+            </Link>
+            </>)}
+        </Menu>
+    )
+
 }
 export default BurgerMenu;
